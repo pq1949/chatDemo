53050900870091d94b5b1eddb35a586ab53d7ba4
pq1949 <2305220598@qq.com>
2018-08-15 19:04:15 +0800

    init

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Message from './message'
 import './index.css'
 
 export default class Content extends Component {
   render() {
     return (
       <div className="content">
-      content
+      <Message />
+      <Message />
       </div>
     )
   }
